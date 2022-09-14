7b4778814d73eba533abc1e7b6f91aff5a157fc8
Anjali <anjalikumari972001@gmail.com>
2022-09-14 23:26:29 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,24 @@
+// html is used to create the visiual element on the  wwb pages
+let todo1="get the groceries";
+let todo ="get the charger";
+let todo2="wash the car";
+let todo3="learn the lessons.";
+console.log(todo);
+let button="click me."
+// javaScript is the case sensitive .
+
+
+// we can create the div using javaScript
+let element=document.createElement('div');// busing document.createElement().
+element.innerText=todo;// todo variable will be added
+document.body.appendChild(element);//adding this div to this page.
+// body is parent and div are the child.
+
+
+
+let element1=document.createElement('div');
+element.innerText=button;
+document.body.appendChild('button');
+
+
+// title.document -----> will chande the title of the document
